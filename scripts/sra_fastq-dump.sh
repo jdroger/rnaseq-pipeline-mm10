c092fe0118082646952693b4b6757bb312231257
jdroger <jdroger@g.clemson.edu>
2021-02-25 15:17:03 -0500

    added fastq-dump for list

--- scripts/sra_fastq-dump.sh
+++ scripts/sra_fastq-dump.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+
 # Shell script for downloading fastq files via SRA-toolkit
 # 02.25.2021 JR
 
-# use prefetch to download sra files
+# define variables
+TOOLKIT=/usr/bin/sratoolkit.2.10.9.ubuntu64/bin
+
+DIR_HOME=/mnt/e/rnaseq/
+DIR_DATA=${DIR_HOME}/data/
+DIR_SAVE=${DIR_DATA}/sra/
 
 # use fastq-dump to convert to fastq
-
-
+for RUN in `cat ${DIR_DATA}/SRR_Acc_List.txt}`;
+do
+	echo $RUN;
+	date;
+	$TOOLKIT/fastq-dump -X 5 -Z ${RUN}; # -O ${DIR_SAVE};
+done
