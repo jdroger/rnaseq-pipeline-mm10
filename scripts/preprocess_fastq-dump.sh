c881206ed54438456c59df76b27bb00569bc426a
jdroger <jdroger@g.clemson.edu>
2021-03-29 15:22:36 -0400

    added usage + check for redundancy

--- scripts/preprocess_fastq-dump.sh
+++ scripts/preprocess_fastq-dump.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Shell script for downloading fastq files via SRA-toolkit
 # 02.25.2021 JR
+# 
+# Usage: `sh preprocess_fastq-dump.sh <list_of_sra_accessions>`
+#   - Uses sratoolkit to download all fastq files specified by list_of_sra_accessions
+#   - Stores all fastq files in data/sra/ directory
+# 
+# Example: `sh preprocess_fastq-dump.sh SRR_Acc_List.txt` downloads 2 fastq files to data/sra/
 #
 # define variables
 TOOLKIT=/usr/bin/sratoolkit.2.10.9-ubuntu64/bin
@@ -8,10 +14,22 @@ DIR_HOME=/mnt/e/rnaseq/
 DIR_DATA=${DIR_HOME}/data/
 DIR_SAVE=${DIR_DATA}/sra/
 #
-# use fastq-dump to convert to fastq
-for RUN in `cat ${DIR_DATA}SRR_Acc_List.txt`; 
+# check if fastq files are already stored in save directory
+for RUN in `cat $1`;
 do
-	echo ${RUN};
-	date;
-	${TOOLKIT}/fastq-dump ${RUN} -O ${DIR_SAVE};
+	FILE = ${DIR_SAVE}/$RUN.fastq
+	if test -f "$FILE"; then
+		echo "$FILE already exists"
+	else
+		echo "downloading $RUN.fastq..."
+		${TOOLKIT}/fastq-dump ${RUN} -O ${DIR_SAVE};
+		echo "fastq saved to $FILE"
+	fi
 done
+# use fastq-dump to convert to fastq
+# for RUN in `cat $1`; 
+# do
+# 	echo ${RUN};
+# 	date;
+# 	${TOOLKIT}/fastq-dump ${RUN} -O ${DIR_SAVE};
+# done
