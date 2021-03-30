5e4d0bf6a0545438f0a72d84b2dcd5933de343ea
jdroger <jdroger@g.clemson.edu>
2021-03-30 15:19:19 -0400

    updated directory + fastqc outputs

--- scripts/preprocess_fastqc.sh
+++ scripts/preprocess_fastqc.sh
@@ -4,18 +4,18 @@
 #
 # define variables
 TOOLKIT=/usr/bin/FastQC/
-DIR_HOME=/mnt/e/rnaseq/
+DIR_HOME=/mnt/e/other-projects/rnaseq/
 DIR_DATA=${DIR_HOME}/data/
 DIR_FASTQ=${DIR_DATA}/sra/
 DIR_SAVE=${DIR_DATA}/fastqc/
 #
 # save names of fastq files to txt file for passing to FastQC
-ls ${DIR_FASTQ}/*.fastq > ${DIR_SAVE}/SRR_FastQC_List.txt
-cat ${DIR_SAVE}/SRR_FastQC_List.txt
+# ls ${DIR_FASTQ}/*.fastq > ${DIR_SAVE}/SRR_FastQC_List.txt
+# cat ${DIR_SAVE}/SRR_FastQC_List.txt
 # 
 # run FastQC on each fastq file + save html files
 # sudo ${TOOLKIT}/fastqc ${DIR_SAVE}/SRR_FastQC_List.txt --outdir=${DIR_SAVE}
-for FILE in ${DIR_FASTQ}/*.fastq;
+for FILE in ${DIR_FASTQ}/*.fastq.gz;
 do
-    ${TOOLKIT}/fastqc ${FILE} --outdir=${DIR_SAVE};
+    ${TOOLKIT}/fastqc ${FILE} --threads 2 --outdir=${DIR_SAVE};
 done
